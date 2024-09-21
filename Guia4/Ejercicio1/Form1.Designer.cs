76c93faf3ce487ce41a59c613da9047a5fcfb6db
castillo1103 <125770998+castillo1103@users.noreply.github.com>
2024-09-20 20:39:13 -0600

    Add files via upload

---
+++ Guia4/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstElementos = new System.Windows.Forms.ListBox();
+            this.btnListaVacia = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstElementos
+            // 
+            this.lstElementos.FormattingEnabled = true;
+            this.lstElementos.ItemHeight = 20;
+            this.lstElementos.Location = new System.Drawing.Point(459, 73);
+            this.lstElementos.Name = "lstElementos";
+            this.lstElementos.Size = new System.Drawing.Size(120, 84);
+            this.lstElementos.TabIndex = 0;
+            // 
+            // btnListaVacia
+            // 
+            this.btnListaVacia.Location = new System.Drawing.Point(92, 73);
+            this.btnListaVacia.Name = "btnListaVacia";
+            this.btnListaVacia.Size = new System.Drawing.Size(124, 62);
+            this.btnListaVacia.TabIndex = 1;
+            this.btnListaVacia.Text = "Crear lista vacía";
+            this.btnListaVacia.UseVisualStyleBackColor = true;
+            this.btnListaVacia.Click += new System.EventHandler(this.btnListaVacia_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(92, 162);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(124, 59);
+            this.btnMostrar.TabIndex = 2;
+            this.btnMostrar.Text = "Mostrar elementos";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(92, 244);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(124, 55);
+            this.btnInsertar.TabIndex = 3;
+            this.btnInsertar.Text = "Insertar elemento";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(92, 321);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(124, 57);
+            this.btnRemover.TabIndex = 4;
+            this.btnRemover.Text = "Remover elemento";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(286, 73);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(100, 26);
+            this.txtElemento.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtElemento);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnListaVacia);
+            this.Controls.Add(this.lstElementos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstElementos;
+        private System.Windows.Forms.Button btnListaVacia;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.TextBox txtElemento;
+    }
+}
+
---
+++ Guia4/Ejercicio1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    { 
+        private int[] lista; // Variable que almacenará el arreglo que simula la lista.
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // a) Crear lista vacía
+        //Este método inicializa el arreglo con una longitud de 0 para simular una lista vacía.
+        private void CrearListaVacia(ref int[] lista)
+        {
+            lista = new int[0]; // Crear una lista (arreglo) vacía.
+        }
+
+        // b) Mostrar listado de elementos de la lista en el ListBox
+        private void MostrarElementos(int[] lista, ListBox listBox)
+        {
+            listBox.Items.Clear(); // Limpiar ListBox antes de mostrar los elementos.
+            foreach (int elemento in lista)
+            {
+                listBox.Items.Add(elemento); // Añadir cada elemento de la lista al ListBox.
+            }
+        }
+
+        // c) Insertar elemento en la lista
+        private void InsertarElemento(ref int[] lista, int nuevoElemento)
+        {
+            int[] nuevaLista = new int[lista.Length + 1]; // Crear un nuevo arreglo con un tamaño mayor.
+            for (int i = 0; i < lista.Length; i++)
+            {
+                nuevaLista[i] = lista[i]; // Copiar los elementos actuales.
+            }
+            nuevaLista[nuevaLista.Length - 1] = nuevoElemento; // Añadir el nuevo elemento al final.
+            lista = nuevaLista; // Asignar la nueva lista al arreglo original.
+        }
+
+        // d) Remover primer elemento de la lista
+        private void RemoverElemento(ref int[] lista)
+        {
+            if (lista.Length > 0)
+            {
+                int[] nuevaLista = new int[lista.Length - 1]; // Crear un nuevo arreglo con un tamaño menor.
+                for (int i = 1; i < lista.Length; i++)
+                {
+                    nuevaLista[i - 1] = lista[i]; // Copiar todos los elementos excepto el primero.
+                }
+                lista = nuevaLista; // Asignar la nueva lista al arreglo original.
+            }
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListaVacia_Click(object sender, EventArgs e)
+        {
+            CrearListaVacia(ref lista); // Llamar al método para crear la lista vacía.
+            MessageBox.Show("Lista vacía creada.");
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            MostrarElementos(lista, lstElementos); // Llamar al método para mostrar los elementos en el ListBox.
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtElemento.Text, out int nuevoElemento))
+            {
+                InsertarElemento(ref lista, nuevoElemento); // Llamar al método para insertar un nuevo elemento.
+                MessageBox.Show($"Elemento {nuevoElemento} agregado.");
+                txtElemento.Clear(); // Limpiar el campo de texto.
+                MostrarElementos(lista, lstElementos); // Mostrar los elementos actualizados.
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número válido.");
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (lista.Length > 0)
+            {
+                RemoverElemento(ref lista); // Llamar al método para remover el primer elemento.
+                MessageBox.Show("Primer elemento removido.");
+                MostrarElementos(lista, lstElementos); // Mostrar los elementos actualizados.
+            }
+            else
+            {
+                MessageBox.Show("No hay elementos para remover.");
+            }
+        }
+    }
+}
